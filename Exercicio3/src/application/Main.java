1b2e8c22676e0abd34d66ee087d5eed17b2dcd74
tainafernandes <tainafefe@gmail.com>
2022-07-06 18:22:04 -0300

    feat: completion of the logic and assembly of the 3 exercise

--- Exercicio3/src/application/Main.java
+++ Exercicio3/src/application/Main.java
@@ -1,7 +1,34 @@
 package application;
 
+import entities.Students;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        Students students = new Students();
+
+        System.out.println("Name: ");
+        students.name = sc.nextLine();
+        System.out.println("Note 1: ");
+        students.note1 = sc.nextDouble();
+        System.out.println("Note 2: ");
+        students.note2 = sc.nextDouble();
+        System.out.println("Note 3: ");
+        students.note3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE = %2.f%n", students.finalGrade());
+
+        if(students.finalGrade() < 60.0){
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f%n", students.missingPoints());
+        } else {
+            System.out.println("PASS");
+        }
+        sc.close();
     }
 }
