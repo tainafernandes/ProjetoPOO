e1884e79f6fdbddf087b00dc8a4728a78f6a00de
tainafernandes <tainafefe@gmail.com>
2022-07-06 17:09:41 -0300

    rectangle entity folder

---
+++ Exercicio1/src/entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Rectangle {
+    public double width; //largura
+    public double height; //altura
+
+    public double area() {
+        return width * height;
+    };
+
+    public double perimeter() {
+        return 2.0 * (width + height);
+    };
+
+    public double
+}
+
+//area = largura * altura
+// perimetro = 2.0 * (largura + altura)
+//diagonal ao quadrado = base ao quadrado + altura ao quadrado ou d = raiz quadrada de base ao quadrado + altura
+//ao quadrado
