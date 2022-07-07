d23c5be102f561e9603a2b5e999995f95689871f
tainafernandes <tainafefe@gmail.com>
2022-07-07 13:35:30 -0300

    feat: Structuring the project

--- Exercicio5/src/application/Main.java
+++ Exercicio5/src/application/Main.java
@@ -1,7 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter account number: ");
+
+        System.out.print("Enter account holder: ");
+
+        System.out.print("Is there na initial deposit (y/n)? ");
+        //vai ter um if?
+        System.out.println("Enter initial deposit value: ");
+
+
+
+        sc.close();
     }
 }
--- Exercicio5/src/service/Account.java
+++ Exercicio5/src/service/Account.java
@@ -1,4 +1,17 @@
 package service;
 
 public class Account {
+    private int accountNumber;
+    private String accountHolder;
+    private double initialDeposit;  //opcional
+    public static double deposit(double amount){
+
+    };
+    public static double withdraw(double amount){
+
+    };
+
+    public String toString(){
+
+    }
 }
