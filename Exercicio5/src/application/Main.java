2c177b65ffd60f6602a857431397ad9b8016d7f0
tainafernandes <tainafefe@gmail.com>
2022-07-07 16:54:56 -0300

    Completion of the proposed exercise for fixing and learning about overload, this, encapsulation, etc.

--- Exercicio5/src/application/Main.java
+++ Exercicio5/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import service.Account;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,16 +9,37 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Account account;
 
         System.out.print("Enter account number: ");
-
+        int number = sc.nextInt();
         System.out.print("Enter account holder: ");
-
+        sc.nextLine(); // para consumir o "espaço" do enter
+        String name = sc.nextLine();
         System.out.print("Is there na initial deposit (y/n)? ");
-        //vai ter um if?
-        System.out.println("Enter initial deposit value: ");
+        char resposta = sc.next().charAt(0);
+        if (resposta == 'y'){
+            System.out.print("Enter initial deposit value: ");
+            double value = sc.nextDouble();
+            account = new Account(number, name, value); //envia esses valores para o contrutor. Construtor é "contruido" com esse valores passados.
+        } else {
+            account = new Account(number, name);
+        }
 
-
+        System.out.println("Account data: ");
+        System.out.print(account);
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double deposite = sc.nextDouble();
+        account.deposit(deposite); //passando o valor que a pessoa deseja depositar.
+        System.out.print("Updated account data: ");
+        System.out.print(account);
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdraw = sc.nextDouble();
+        account.withdraw(withdraw);
+        System.out.print("Updated account data: ");
+        System.out.print(account);
 
         sc.close();
     }
--- Exercicio5/src/service/Account.java
+++ Exercicio5/src/service/Account.java
@@ -4,14 +4,38 @@ public class Account {
     private int accountNumber;
     private String accountHolder;
     private double initialDeposit;  //opcional
-    public static double deposit(double amount){
 
+    public Account (int accountNumber, String accountHolder, double initialDeposit){ //Construtor caso opte por fazer o depósito inicial
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        deposit(initialDeposit); //construtor serve para uma inicialização do objeto. Oobjeto existir de fato.
+    }
+
+    public Account (int accountNumber, String accountHolder) { //Construtor caso a pessoa não queria fazer o depósito inicial
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public int getAccountNumber(){ //get só pega valores.
+        return accountNumber;
+    }
+    public void setAccountHolder(String accountHolder){ //void - não sabe o que vai modificar - retorna o que é passado
+         this.accountHolder = accountHolder; //se refere ao parametro privado, passa para o paraetro privado o que está sendo alterado
+    }
+    public String getAccountHolder(){
+        return accountHolder;
+    }
+    public double getInitialDeposit(){
+        return initialDeposit;
+    }
+    public void deposit(double amount){
+        initialDeposit += amount;
     };
-    public static double withdraw(double amount){
-
+    public void withdraw(double amount){
+        initialDeposit -= amount + 5.00;
     };
 
     public String toString(){
-
+        return "Account " + accountNumber + ", Holder: " + accountHolder + ", Balance: $ " + String.format("%.2f", initialDeposit);
     }
 }
