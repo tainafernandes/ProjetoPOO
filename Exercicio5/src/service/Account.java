be6b44cdf9500cc7fed51a3fbe02b8e553c1de7a
tainafernandes <tainafefe@gmail.com>
2022-07-08 08:37:18 -0300

    feat: code improvement modifications

--- Exercicio5/src/service/Account.java
+++ Exercicio5/src/service/Account.java
@@ -6,34 +6,42 @@ public class Account {
     private double initialDeposit;  //opcional
 
     public Account (int accountNumber, String accountHolder, double initialDeposit){ //Construtor caso opte por fazer o depósito inicial
-        this.accountNumber = accountNumber;
-        this.accountHolder = accountHolder;
+        setAccountNumber(accountNumber);
+        setAccountHolder(accountHolder);
         deposit(initialDeposit); //construtor serve para uma inicialização do objeto. Oobjeto existir de fato.
     }
 
     public Account (int accountNumber, String accountHolder) { //Construtor caso a pessoa não queria fazer o depósito inicial
-        this.accountNumber = accountNumber;
-        this.accountHolder = accountHolder;
+        setAccountNumber(accountNumber);
+        setAccountHolder(accountHolder);
     }
 
-    public int getAccountNumber(){ //get só pega valores.
+    private void setAccountNumber(int accountNumber){ //modificador de acesso - tipo de retorno - nome função - (tipo de entrada - parametros)
+        this.accountNumber = accountNumber;
+    }
+    public int getAccountNumber(){ //get retorna valores para quem chamou.
         return accountNumber;
     }
-    public void setAccountHolder(String accountHolder){ //void - não sabe o que vai modificar - retorna o que é passado
-         this.accountHolder = accountHolder; //se refere ao parametro privado, passa para o paraetro privado o que está sendo alterado
+
+    public void setAccountHolder(String accountHolder){ //void - não tem retorno.
+         this.accountHolder = accountHolder; //se refere ao parametro privado, passa para o parametro privado o que está sendo alterado
     }
     public String getAccountHolder(){
         return accountHolder;
     }
+
     public double getInitialDeposit(){
         return initialDeposit;
     }
+
     public void deposit(double amount){
         initialDeposit += amount;
     };
+
     public void withdraw(double amount){
         initialDeposit -= amount + 5.00;
     };
+
 
     public String toString(){
         return "Account " + accountNumber + ", Holder: " + accountHolder + ", Balance: $ " + String.format("%.2f", initialDeposit);
