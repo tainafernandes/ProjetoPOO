7027e1ab0190264366a740185c04404ce7063fae
tainafernandes <tainafefe@gmail.com>
2022-07-20 13:11:33 -0300

    end of exercise + use of stringBuilder

--- Exercicio9/src/entities/Comment.java
+++ Exercicio9/src/entities/Comment.java
@@ -2,5 +2,19 @@ package entities;
 
 public class Comment {
 
+    private String text;
+    public Comment() {
+        //empty constructor
+    }
 
+    public Comment(String text){
+        this.text = text;
+    }
+    public String getText(){
+        return text;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
 }
