598e592d62d1d89008c55c27bfd92b7024fbecf6
tainafernandes <tainafefe@gmail.com>
2022-07-20 08:07:31 -0300

    Thinking about structuring the challenge

--- Desafio1/src/application/main.java
+++ Desafio1/src/application/main.java
@@ -1,7 +1,18 @@
 package application;
 
+import entities.Quiz;
+
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Quiz quiz = new Quiz();
+
+        System.out.println("Bem vindo ao Quiz!");
+        System.out.println("Esse quiz é composto por " + String.format("%.0f", quiz.question())+ " Questoes");
+
+        sc.close();
     }
 }
--- Desafio1/src/entities/Alternativa.java
+++ Desafio1/src/entities/Alternativa.java
@@ -7,8 +7,19 @@ public class Alternativa {
     private boolean correta;
 
     public Alternativa (char letra, String resposta, boolean correta){
-
+        this.letra = letra;
+        this.resposta = resposta;
+        this.correta = correta;
     }
+
+    public char getLetra(){
+        return letra;
+    }
+
+    public void setLetra(char letra){
+        this.letra = letra;
+    }
+
 
 
 }
