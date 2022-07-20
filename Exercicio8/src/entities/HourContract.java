0c146716a1092a8c1108b5e38eef34724c8bc160
tainafernandes <tainafefe@gmail.com>
2022-07-20 10:12:37 -0300

    creation of the totalValue function

--- Exercicio8/src/entities/HourContract.java
+++ Exercicio8/src/entities/HourContract.java
@@ -1,2 +1,47 @@
-package entities;public class HourContract {
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+    private Date date;
+    private double valuePerHour;
+    private int hours;
+
+    public HourContract(){
+        //empty constructor
+    }
+
+    public HourContract(Date date, double valuePerHour, int hours){
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public void setDate(Date date) {
+       this.date = date;
+    }
+
+    public double getValuePerHour(){
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(double valuePerHour){
+        this.valuePerHour = valuePerHour;
+    }
+
+    public int hours(){
+        return hours;
+    }
+
+    public void setHours(int hours){
+        this.hours = hours;
+    }
+
+    public double totalValue(){ //total do contrato
+        return valuePerHour * hours;
+    }
 }
