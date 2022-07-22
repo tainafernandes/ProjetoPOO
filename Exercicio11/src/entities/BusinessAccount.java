adf52a0448272fbea81c909db97406226ac7d24f
tainafernandes <tainafefe@gmail.com>
2022-07-22 07:48:10 -0300

    Demonstration exercise on inheritance and up and downcasting

---
+++ Exercicio11/src/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class BusinessAccount extends Account{ //herança
+
+    private Double loanLimit;
+
+    public BusinessAccount(){
+        super(); //coloca caso o construtor padrao do Account modificar, ele modifica o do business
+        //empty constructor
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance); //o super serve para mostrar que pega informações da superclasse
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if(amount <= loanLimit) {
+            deposit(amount);
+        }
+    }
+}
