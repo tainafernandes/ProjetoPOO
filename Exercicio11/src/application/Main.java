406936edbb2e0ee2051fa55473cfad487f655f56
tainafernandes <tainafefe@gmail.com>
2022-07-22 08:10:26 -0300

    Examples of up and downcasting

--- Exercicio11/src/application/Main.java
+++ Exercicio11/src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,6 +12,30 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Account acc = new Account(1001, "Alex",0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+
+        Account acc1 = bacc; //relacao é-um - BusinessAccount é uma conta, então posso atribuir
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        //DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2; //Incompatibilidade - não posso converter
+        //para funcionar faço o casting manual.
+        //No downcasting nem sempre da certo. Preciso testar
+        if(acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
 
 
         sc.close();
