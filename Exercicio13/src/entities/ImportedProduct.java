e416d380c5e6723dffd1018103d712cef32407b1
tainafernandes <tainafefe@gmail.com>
2022-07-25 08:52:26 -0300

    Finalization of the ImportedProduct class

--- Exercicio13/src/entities/ImportedProduct.java
+++ Exercicio13/src/entities/ImportedProduct.java
@@ -1,4 +1,33 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+    protected Double customsFree;
+
+    public ImportedProduct(){
+        super();
+    }
+    public ImportedProduct(String name, Double price, Double customsFree) {
+        super(name, price);
+        this.customsFree = customsFree;
+    }
+
+    public Double getCustomsFree() {
+        return customsFree;
+    }
+
+    public void setCustomsFree(Double customsFree) {
+        this.customsFree = customsFree;
+    }
+    @Override
+    public String priceTag(){
+        return name
+                + " $ "
+                + String.format("%.2f", price)
+                + " (Customs fee: $ "
+                + String.format("%.2f", customsFree) + ")";
+    }
+
+    public Double totalPrice(){
+        return super.price + customsFree;
+    }
 }
