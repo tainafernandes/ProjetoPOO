15193b7b7dcc6509d65692e8a2796af44d5b3b43
tainafernandes <tainafefe@gmail.com>
2022-07-25 12:17:14 -0300

    End of exercise

--- Exercicio15/src/application/Main.java
+++ Exercicio15/src/application/Main.java
@@ -1,5 +1,11 @@
 package application;
 
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,8 +14,42 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List<TaxPayer> list = new ArrayList<>();
 
+        System.out.print("Enter the number of tax payers: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.print("Tax payer #" + i + " data: ");
+            System.out.print("Individual or company (i/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Anual income: ");
+            double anualIncome = sc.nextDouble();
+            if (ch == 'i'){
+                System.out.print("Health expenditures: ");
+                double healthExpenditures = sc.nextDouble();
+                list.add(new Individual(name, anualIncome, healthExpenditures));
+            } else {
+                System.out.print("Number of employees: ");
+                int numberOfEmployess = sc.nextInt();
+                list.add(new Company(name, anualIncome, numberOfEmployess));
+            }
+        }
 
+        System.out.println();
+        System.out.println("TAXES PAID");
+        for(TaxPayer tp : list){
+            System.out.println(tp.getName() + ": $ " + String.format("%.2f", tp.tax()));
+        }
+        System.out.println();
+        double sum = 0.0;
+        for(TaxPayer tp : list){
+            sum += tp.tax();
+        }
+
+        System.out.print("TOTAL TAXES: $ " + String.format("%.2f", sum));
         sc.close();
     }
 }
