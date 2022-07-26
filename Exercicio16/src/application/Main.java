cf891b9ab2082be8daa9e94b99b4b3476f4c8c05
tainafernandes <tainafefe@gmail.com>
2022-07-26 10:36:30 -0300

    Made the Account functions and assembled the basic program in Main

--- Exercicio16/src/application/Main.java
+++ Exercicio16/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Account;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +9,17 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        String holder = sc.next();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+        Account ac = new Account(number, holder, balance, withdrawLimit);
 
 
 
--- Exercicio16/src/entities/Account.java
+++ Exercicio16/src/entities/Account.java
@@ -6,4 +6,54 @@ public class Account {
     protected Double balance;
     protected Double withdrawLimit;
 
+    public Account(){
+        //empty constructor
+    }
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Double deposit(Double amount){
+        return balance += amount;
+    }
+
+    public Double withdraw(Double amount){
+        return balance += amount;
+    }
 }
