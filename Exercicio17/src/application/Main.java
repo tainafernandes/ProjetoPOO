bcbf8cd25e2b11a81017bab00e2461e01736d28a
tainafernandes <tainafefe@gmail.com>
2022-07-27 17:21:05 -0300

    Example using FileWriter and BufferedWriter

--- Exercicio17/src/application/Main.java
+++ Exercicio17/src/application/Main.java
@@ -1,12 +1,28 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Usando FileWriter e Buffered Writer
+        String[] lines = new String[]{"Good nigth", "Welcome"};
+
+        String path = "/home/taina-fernandes/ProjetosJava/Projeto_POO/Exercicio17/index.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine(); // quebra de linha
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+        /*
         //exemplo com o bloco try-with-resources
             String path = "/home/taina-fernandes/ProjetosJava/Projeto_POO/Exercicio17/index.txt";
             try (BufferedReader br = new BufferedReader(new FileReader(path))) {
@@ -18,7 +34,7 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("Error: " + e.getMessage());
             }
-        }
+        } */
 
 
         /*
