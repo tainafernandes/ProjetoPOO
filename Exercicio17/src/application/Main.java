89408239e34f229b30df0ce2c608d7ccdd45a589
tainafernandes <tainafefe@gmail.com>
2022-07-27 17:12:31 -0300

    Example using try-with-resources block

--- Exercicio17/src/application/Main.java
+++ Exercicio17/src/application/Main.java
@@ -1,11 +1,28 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //exemplo com o bloco try-with-resources
+            String path = "/home/taina-fernandes/ProjetosJava/Projeto_POO/Exercicio17/index.txt";
+            try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+                String line = br.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = br.readLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+
+        /*
+        MEXENDO SOMENTE COM O FILE
         File file = new File("/home/taina-fernandes/ProjetosJava/Projeto_POO/Exercicio17/index.txt");
         Scanner sc = null;
         try {
@@ -20,5 +37,5 @@ public class Main {
                 sc.close();
             }
         }
-    }
+    } */
 }
