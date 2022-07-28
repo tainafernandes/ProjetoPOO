3c1150a60d020bc1c26ee58a65966e33f25d90db
tainafernandes <tainafefe@gmail.com>
2022-07-28 10:36:01 -0300

    Made the PaypalService class that implements from OnlinePaymentService

--- Exercicio19/src/model/services/PaypalService.java
+++ Exercicio19/src/model/services/PaypalService.java
@@ -1,4 +1,16 @@
 package model.services;
 
-public class PaypalService {
+public class PaypalService implements OnlinePaymentService{
+    private static final double PAYMENT_FEE = 0.02; //hard conding
+    private static final double MONTHLY_INTEREST = 0.01;
+
+    @Override
+    public Double paymentFee(double amount) {
+        return amount * PAYMENT_FEE;
+    }
+
+    @Override
+    public Double interest(double amount, int months) {
+        return amount * months * MONTHLY_INTEREST;
+    }
 }
