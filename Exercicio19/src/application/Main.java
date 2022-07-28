03ab73a1e89b1b449119b28328fd7ccff0c2fd87
tainafernandes <tainafefe@gmail.com>
2022-07-28 10:45:59 -0300

    Using the Dependency Injection Principle

--- Exercicio19/src/application/Main.java
+++ Exercicio19/src/application/Main.java
@@ -1,12 +1,15 @@
 package application;
 
+import model.services.OnlinePaymentService;
+import model.services.PaypalService;
+
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Programa principal que vai ficar responsável em injetar e não mais a classe
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
--- Exercicio19/src/model/services/ContractService.java
+++ Exercicio19/src/model/services/ContractService.java
@@ -1,4 +1,16 @@
 package model.services;
 
 public class ContractService {
+
+    //Vamos ter composição de serviços - Classe fechada para alteração e aberta para inclusão
+    //Inversão de controle é NÃO deixar a classe responsavel em instanciar. Outro que vai dar o NEW (instanciar)
+    //De forma manual é um constructor, get
+
+    private OnlinePaymentService onlinePaymentService; //declara com o tipo da interface
+
+    public ContractService(OnlinePaymentService onlinePaymentService){ //INJETANDO
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+
 }
